980353322ed66a5dbbc90238526efd98d7826e8c
johney <johneyburke@gmail.com>
2023-08-20 14:12:04 -0600

    used git add

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+
+    cats.push("Ralph");
+
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+
+}
+
+function destructivelyRemoveLastCat(){
+
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+
+    cats.shift();
+}
+
+function  appendCat(name){
+
+    const newCats = [ ...cats, name];
+    return newCats;
+}
+
+
+function  prependCat(name){
+
+    const moreCats = [ name, ...cats];
+    return moreCats;
+}
+
+
+function  removeLastCat(name){
+
+    const lessCats = cats.slice(0, cats.length-1);
+    return lessCats;
+}
+
+function  removeFirstCat(name){
+
+    const evenLessCats = cats.slice(1);
+    return evenLessCats;
+}
